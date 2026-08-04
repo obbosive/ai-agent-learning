d0a5c3e87c5619a84229ddb30aa64890f8aa196d
zhangxuesen <2385424740@qq.com>
2026-08-04 16:58:35 +0800

    day1:add first python program

---
+++ day01/hello.py
@@ -0,0 +1,14 @@
+goal = "学会独立编程，找到AI Agent相关实习"
+graduation_year = "2028"
+direction = "AI Agent独立开发"
+days = 7
+hours_per_day=4
+total_hours=hours_per_day*days
+
+print("我的学习目标：",goal)
+print("我的毕业年份",graduation_year)
+print("目标职业：",direction)
+print("本计划执行时间：",days)
+
+print("每天学习时间：",hours_per_day)
+print("每周总学习时间：",total_hours)
